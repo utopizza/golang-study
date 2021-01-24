d90f2abb618e4527e74c54fad8f27b604230516c
YushengWxxx <648847079@qq.com>
2021-01-24 20:06:13 +0800

    add more functions for BST

--- algorithm/search/bst.go
+++ algorithm/search/bst.go
@@ -52,3 +52,115 @@ func get(n *node, key string) interface{} {
 		return get(n.left, key)
 	}
 }
+
+// Delete 删除指定key的元素
+func (b *BinarySearchTree) Delete(key string) {
+	delete(b.root, key)
+}
+
+func delete(n *node, key string) *node {
+	if n == nil {
+		return nil
+	} else if n.key < key {
+		n.right = delete(n.right, key)
+		return n
+	} else if n.key > key {
+		n.left = delete(n.left, key)
+		return n
+	} else {
+		// 当前节点为要删除的结点
+		if n.left == nil {
+			// 若无左孩，用右孩替代本结点
+			return n.right
+		} else if n.right == nil {
+			// 若无右孩，用左孩替代本结点
+			return n.left
+		} else {
+			// 若既有左孩又有右孩，则需用[前驱结点]，或者[后继结点]替代本结点
+			// [前驱结点]即为左子树的最大结点
+			// [后继结点即]为右子树的最小结点
+			x := min(n.right)
+			n = deleteMin(n)
+			x.left = n.left
+			x.right = n.right
+			return x
+		}
+	}
+}
+
+// Min 返回最小key以及value
+func (b *BinarySearchTree) Min() (string, interface{}) {
+	n := min(b.root)
+	return n.key, n.value
+}
+
+func min(n *node) *node {
+	if n == nil {
+		return nil
+	}
+	for n.left != nil {
+		n = n.left
+	}
+	return n
+}
+
+func deleteMin(n *node) *node {
+	if n == nil {
+		return nil
+	} else if n.left == nil {
+		return n.right
+	} else {
+		n.left = deleteMin(n.left)
+		return n
+	}
+}
+
+// Max 返回最大key以及value
+func (b *BinarySearchTree) Max() (string, interface{}) {
+	n := max(b.root)
+	return n.key, n.value
+}
+
+func max(n *node) *node {
+	if n == nil {
+		return nil
+	}
+	for n.right != nil {
+		n = n.right
+	}
+	return n
+}
+
+func deleteMax(n *node) *node {
+	if n == nil {
+		return nil
+	} else if n.right == nil {
+		return n.left
+	} else {
+		n.right = deleteMax(n.right)
+		return n
+	}
+}
+
+// Keys 获取全部key，按大小顺序
+func (b *BinarySearchTree) Keys() []string {
+	var keys []string
+	inorder(b.root, keys)
+	return keys
+}
+
+func inorder(n *node, keys []string) {
+	if n == nil {
+		return
+	}
+	// left tree
+	if n.left != nil {
+		inorder(n.left, keys)
+	}
+	// current node
+	keys = append(keys, n.key)
+	// right tree
+	if n.right != nil {
+		inorder(n.right, keys)
+	}
+}
--- algorithm/search/bst_test.go
+++ algorithm/search/bst_test.go
@@ -1,17 +1,63 @@
 package search
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestBST(t *testing.T) {
 	bst := NewBinarySearchTree()
+
+	// put
 	bst.Put("b", 2)
 	bst.Put("a", 1)
 	bst.Put("c", 3)
 	bst.Put("d", 4)
+
+	// get
 	if v := bst.Get("c"); v.(int) != 3 {
 		t.Error(v)
 	}
 	if v := bst.Get("d"); v.(int) != 4 {
 		t.Error(v)
 	}
+	if v := bst.Get("f"); v != nil {
+		t.Error(v)
+	}
+
+	// keys
+	expectedKeys := []string{"a", "b", "c", "d"}
+	actualKeys := bst.Keys()
+	if equal(actualKeys, expectedKeys) {
+		t.Error(actualKeys)
+	}
+
+	// min
+	if k, _ := bst.Min(); k != "a" {
+		t.Error(k)
+	}
+
+	// max
+	if k, _ := bst.Max(); k != "d" {
+		t.Error(k)
+	}
+
+	// detele
+	bst.Delete("c")
+	expectedKeys = []string{"a", "b", "d"}
+	actualKeys = bst.Keys()
+	if equal(actualKeys, expectedKeys) {
+		t.Error(actualKeys)
+	}
+}
+
+func equal(arr []string, target []string) bool {
+	if len(arr) != len(target) {
+		return false
+	}
+	for i := range arr {
+		if arr[i] != target[i] {
+			return false
+		}
+	}
+	return true
 }
