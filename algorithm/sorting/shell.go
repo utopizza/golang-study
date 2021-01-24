e0c3e8b22055d24e8581fbfc189b84a167ef92f4
YushengWxxx <648847079@qq.com>
2021-01-24 12:21:24 +0800

    add shell sort

---
+++ algorithm/sorting/shell.go
@@ -0,0 +1,16 @@
+package sorting
+
+func shellSort(arr []int) {
+	h := 1
+	for 3*h < len(arr) {
+		h = 3*h + 1
+	}
+	for h >= 1 {
+		for i := range arr {
+			for j := i; j >= h && arr[j] < arr[j-h]; j -= h {
+				swap(arr, j, j-h)
+			}
+		}
+		h = h / 3
+	}
+}
---
+++ algorithm/sorting/shell_test.go
@@ -0,0 +1,14 @@
+package sorting
+
+import (
+	"testing"
+)
+
+func TestShellSort(t *testing.T) {
+	source1 := []int{6, 5, 4, 3, 2, 1}
+	target1 := []int{1, 2, 3, 4, 5, 6}
+	shellSort(source1)
+	if !equal(source1, target1) {
+		t.Error(source1)
+	}
+}
